26df3b49ad59e976e8ba65eadb86d328d7e47310
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-02-13 21:59:02 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python  
-#-*- coding:utf-8 _*-  
-""" 
+#!/usr/bin/env python
+#-*- coding:utf-8 _*-
+"""
 @author:liruihui
-@file: train.py 
+@file: train.py
 @time: 2019/09/17
 @contact: ruihuili.lee@gmail.com
 @github: https://liruihui.github.io/
-@description: 
+@description:
 """
 import os
 import pprint
@@ -19,8 +19,8 @@ from Generation.config import opts
 if __name__ == '__main__':
 
 
-    opts.pretrain_model_G = "table_G.pth"
-    opts.log_dir = "models"
+    opts.pretrain_model_G = "Chair_G.pth"
+    opts.log_dir = "trained_models"
 
     model = Model(opts)
 
@@ -34,6 +34,3 @@ if __name__ == '__main__':
     #model.draw_edit_inte() # combine for part edit & part/shape interpolate
 
     #model.draw_part_exchange() # exchange the noise vector of two regions of two shape
-
-
-
